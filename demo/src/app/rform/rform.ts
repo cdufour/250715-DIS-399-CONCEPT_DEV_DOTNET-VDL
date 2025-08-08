054202e2a73baee4d81b78ae65cb9e0ffa3287bc
unknown <opusidea@gmail.com>
2025-08-08 10:49:16 +0200

    j5 TP

--- demo/src/app/rform/rform.ts
+++ demo/src/app/rform/rform.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-rform',
   templateUrl: './rform.html',
-  standalone: true
+  standalone: true,
+  imports: [ReactiveFormsModule]
 })
 export class Rform {
-
+  studentForm: FormGroup = new FormGroup({
+    name: new FormControl('', Validators.required),
+    grade: new FormControl('10', [Validators.min(0), Validators.max(20)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    postcode: new FormControl('', [Validators.required, Validators.pattern(/\d{5}/g)]),
+  })
 }
